3535c6bda9c0d307b8593091eb096a7e2f2d471b
shivamgupta02 <shivam.g@taltrip.com>
2024-08-05 14:14:34 +0530

    change file name

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
   if (
-    (token &&
+    (!stoken &&
       (url.pathname.startsWith("/sign-in") ||
         url.pathname.startsWith("/sign-up") ||
         url.pathname.startsWith("/verify"))) ||
